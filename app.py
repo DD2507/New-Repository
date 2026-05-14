59ba377c6cc4af54a8b648c4b495a44be2d50e20
Dhrupad <dhrupad2005@gmail.com>
2026-05-14 12:35:39 +0530

    Testing rollback

--- app.py
+++ app.py
@@ -1,11 +1,52 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import requests
 import os
 
 app = Flask(__name__)
 
+JENKINS_URL = "http://localhost:9090"
+JOB_NAME = "auto-rollback-pipeline"
+USERNAME = "dhrupad"
+API_TOKEN = "1150c13ed47776caf8dd6aef02f2c008a7"
+
 @app.route("/")
 def home():
-    return "Version 1: Application Running Successfully!"
+    return render_template("index.html")
+
+@app.route("/trigger-update", methods=["POST"])
+def trigger_update():
+
+    build_url = f"{JENKINS_URL}/job/{JOB_NAME}/build"
+
+    try:
+
+        response = requests.post(
+            build_url,
+            auth=(USERNAME, API_TOKEN)
+        )
+
+        print("STATUS:", response.status_code)
+        print("RESPONSE:", response.text)
+
+        if response.status_code in [200, 201]:
+            return jsonify({
+                "status": "success",
+                "message": "Jenkins pipeline triggered successfully"
+            })
+
+        return jsonify({
+            "status": "failed",
+            "message": f"Jenkins returned status {response.status_code}"
+        }), 500
+
+    except Exception as e:
+
+        print("ERROR:", str(e))
+
+        return jsonify({
+            "status": "failed",
+            "message": str(e)
+        }), 500
 
 @app.route("/health")
 def health():
